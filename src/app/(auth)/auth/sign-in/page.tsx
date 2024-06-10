f3b4f5f2c8c34072d5e82515ad31365d3aefdd40
michaeltaman <mictrwork@gmail.com>
2024-06-10 11:20:10 +0300

    Added email verification

--- src/app/(auth)/auth/sign-in/page.tsx
+++ src/app/(auth)/auth/sign-in/page.tsx
@@ -1,3 +1,40 @@
-export default function SignIn() {
-    return <div className="container py-10">Sign In</div>
+import { SignInForm } from "@/features/auth/sign-in-form.server";
+import { Card, CardContent, CardHeader } from "@/shared/ui/card";
+import Link from "next/link";
+
+export default function AuthenticationPage() {
+  return (
+    <>
+      {" "}
+      <div className="container relative  flex-col items-center justify-center self-center pt-24">
+        <Card className="max-w-[350px] mx-auto">
+          <CardHeader className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+                Login to account
+            </h1>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <SignInForm />
+            <p className="px-0 text-center text-sm text-muted-foreground">
+                By clicking continue you agree to{" "}
+              <Link
+                href="/terms"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                User Agreement
+              </Link>{" "}
+              и{" "}
+              <Link
+                href="/privacy"
+                className="underline underline-offset-4 hover:text-primary"
+              >
+                Privacy Policy
+              </Link>
+              .
+            </p>
+          </CardContent>
+        </Card>
+      </div>
+    </>
+  );
 }
