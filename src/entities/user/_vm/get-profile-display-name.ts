b63bfe8b380a4125d102eadabf5f3b7781f6c407
michaeltaman <mictrwork@gmail.com>
2024-06-16 17:52:02 +0300

    receive profile_2

--- src/entities/user/_vm/get-profile-display-name.ts
+++ src/entities/user/_vm/get-profile-display-name.ts
@@ -5,5 +5,9 @@ import { Profile } from '../_domain/types';
 // };
 
 export const getProfileDisplayName = (profile: Profile) => {
-  return profile.name && profile.name.length > 1 ? profile.name : profile.email;
+  return profile.name && profile.name.length > 1
+    ? profile.name
+    : profile.email
+      ? profile.email
+      : '--';
 };
