a316875666ad5866b24b4837639421d3caa29a3e
michaeltaman <mictrwork@gmail.com>
2024-06-18 19:07:23 +0300

    added uploadAvatarAction

--- src/features/update-profile/_ui/avatar-field.tsx
+++ src/features/update-profile/_ui/avatar-field.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/shared/ui/button';
 import { Spinner } from '@/shared/ui/spinner';
 import { ProfileAvatar } from '@/entities/user/profile';
+import { useUploadAvatar } from '../_vm/use-upload-avatar';
 
 export function AvatarField({
   id,
@@ -20,13 +21,16 @@ export function AvatarField({
     }
   };
 
+  const {handleFileSelect, isPending} = useUploadAvatar({});
+
   return (
     <Button
       variant="ghost"
       className="w-[84px] h-[84px] p-0.5 rounded-full relative block"
       type="button"
+      onClick = {handleFileSelect}
     >
-      {false && (
+      {isPending && (
         <div className="inset-0 absolute flex items-center justify-center z-10">
           <Spinner className="w-10 h-10" aria-label="New avatar loading ..." />
         </div>
